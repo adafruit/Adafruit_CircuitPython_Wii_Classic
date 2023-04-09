23b67ae9a587ce44a965a90dcb2d30606c9700b2
BlitzCityDIY <lizillah@REPTAR>
2023-04-09 10:30:13 -0400

    updating examples

--- examples/wii_classic_simpletest.py
+++ examples/wii_classic_simpletest.py
@@ -12,8 +12,8 @@ ctrl_pad = adafruit_wii_classic.Wii_Classic(i2c)
 while True:
     left_x, left_y = ctrl_pad.joystick_l
     right_x, right_y = ctrl_pad.joystick_r
-    left_pressure = ctrl_pad.l_shoulder
-    right_pressure = ctrl_pad.r_shoulder
+    left_pressure = ctrl_pad.l_shoulder.LEFT_FORCE
+    right_pressure = ctrl_pad.r_shoulder.RIGHT_FORCE
     print("joystick_l = {},{}".format(left_x, left_y))
     print("joystick_r = {},{}".format(right_x, left_y))
     print("left shoulder = {}".format(left_pressure))
@@ -23,5 +23,11 @@ while True:
     if ctrl_pad.buttons.B:
         print("button B")
     if ctrl_pad.d_pad.UP:
-        print("button Up")
+        print("dpad Up")
+    if ctrl_pad.d_pad.DOWN:
+        print("dpad Down")
+    if ctrl_pad.d_pad.LEFT:
+        print("dpad Left")
+    if ctrl_pad.d_pad.RIGHT:
+        print("dpad Right")
     time.sleep(0.5)
